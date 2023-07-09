58edeb6c12046d87ecaa05037bdeb06c70adaa8c
Dmitry Shakhin <dshahin@yandex.ru>
2023-07-09 16:07:35 +0300

    Выходной или рабочий день

---
+++ Seminar 2/Task 15/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным
+
+System.Console.Write("Введите номер дня недели: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num == 6 || num == 7)
+{
+    System.Console.WriteLine("Это выходной день!");
+}
+else if (num >= 1 && num <= 5)
+{
+    System.Console.WriteLine("Это рабочий день");
+}
+else
+{
+    System.Console.WriteLine("Вы ввели неправильный номер. В неделе 7 дней.");
+}
