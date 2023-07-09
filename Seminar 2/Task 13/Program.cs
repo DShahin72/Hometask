722d2481afa671332beb3f5003ff7cba65e8b552
Dmitry Shakhin <dshahin@yandex.ru>
2023-07-09 15:50:38 +0300

    Вывод третьей цифры введенного числа

---
+++ Seminar 2/Task 13/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+System.Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num / 100 == 0)
+{
+    System.Console.WriteLine($"В числе {num} нет третьей цифры");
+}
+
+int N = num;
+int fig3 = num % 10;
+int fig2 = (num / 10) % 10;
+int fig1 = (num / 100) % 10;
+//System.Console.WriteLine($"{fig1}, {fig2}, {fig3}");
+
+while (num / 1000 != 0)
+{
+    num = num / 10;
+    fig1 = fig2;
+    fig2 = fig3;
+    fig3 = num % 10;
+}
+
+System.Console.WriteLine($"Третья цифра числа {N} равна {fig3}");
